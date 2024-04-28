39cabdc049dc2f226c7ea752c80cef44c5be86ee
Rashad Ataf <rashadataf@gmail.com>
2024-04-28 19:21:28 +0100

    feat: add shared package to provide shared features

---
+++ apps/shared/src/enums.ts
@@ -0,0 +1,8 @@
+export enum ConfigKey {
+    SERVER_PORT = "port",
+    DATABASE_HOST = "database.host",
+    DATABASE_NAME = "database.name",
+    DATABASE_USER = "database.user",
+    DATABASE_PASSWORD = "database.password",
+    DATABASE_PORT = "database.port",
+}
---
+++ apps/shared/src/types.ts
@@ -0,0 +1,12 @@
+export type DatabaseConfig = {
+    host: string;
+    name: string;
+    user: string;
+    password: string;
+    port: number;
+}
+
+export type ServerConfig = {
+    port: number;
+    database: DatabaseConfig;
+}
