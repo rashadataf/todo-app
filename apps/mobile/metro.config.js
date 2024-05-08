7049c93b84be7b9fb270141456328d08037dde27
Rashad Ataf <rashadataf@gmail.com>
2024-05-08 07:43:33 +0100

    fix: modify metro configa to meet monorepo configs

--- apps/mobile/metro.config.js
+++ apps/mobile/metro.config.js
@@ -1,11 +1,25 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const path = require('path');
 
+const projectRoot = __dirname;
+const monorepoRoot = path.resolve(projectRoot, '../..');
 /**
  * Metro configuration
  * https://reactnative.dev/docs/metro
  *
  * @type {import('metro-config').MetroConfig}
  */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const extraConfig = {};
+
+defaultConfig.watchFolders = [monorepoRoot];
+
+defaultConfig.resolver.nodeModulesPaths = [
+    path.resolve(projectRoot, 'node_modules'),
+    path.resolve(monorepoRoot, 'node_modules'),
+];
+
+const config = mergeConfig(defaultConfig, extraConfig);
+
+module.exports = config;
