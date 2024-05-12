5b2b15b4bfa3c60638cb2c5c68658f47d4d0002a
Rashad Ataf <rashadataf@gmail.com>
2024-05-12 12:07:49 +0100

    feat: add database module and establish the connection

---
+++ apps/backend/src/database/database.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { NodeEnvironment } from '@workspace/shared';
+import { ConfigService } from '../config/config.service';
+
+@Module({
+    imports: [
+        TypeOrmModule.forRootAsync({
+            extraProviders: [ConfigService],
+            inject: [ConfigService],
+            useFactory: async (config: ConfigService) => ({
+                type: 'postgres',
+                host: config.databaseHost,
+                port: config.databasePort,
+                username: config.databaseUser,
+                password: config.databasePassword,
+                database: config.databaseName,
+                entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+                synchronize: config.nodeEnvironment !== NodeEnvironment.PRODUCTION,
+                migrationsRun: true,
+                migrations: [__dirname + '/migrations/**/*{.ts,.js}']
+            }),
+        }),
+    ],
+})
+export class DatabaseModule { }
